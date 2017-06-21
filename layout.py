bdb2b8bba6d513b53994b554acc2ecc035503f74
lazukr <peterh10201@gmail.com>
2017-06-21 21:33:02 +0200

    sample pyKiwi

---
+++ layout.py
@@ -0,0 +1,13 @@
+# this module implements layouts
+
+
+class Window:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
