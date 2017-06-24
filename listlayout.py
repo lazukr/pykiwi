fb7ddc4e66610982919cd74e0b00b9523a18ba8c
lazukr <peterh10201@gmail.com>
2017-06-24 07:12:21 +0200

    added orientation and autoscale on listlayout

--- listlayout.py
+++ listlayout.py
@@ -1,24 +1,53 @@
 from layout import Layout
 from pos import Pos
+from size import Size
 
 import pygame
 import colour
 
 class ListLayout(Layout):
-    def __init__(self, size, pos=Pos(0,0)):
-        super(ListLayout, self).__init__(size, pos)
+    def __init__(self, size, pos=Pos(0,0), border=5, orientation="vertical", spacing=2):
+        super(ListLayout, self).__init__(size, pos, border)
         self.next_child_origin = Pos(
-            self.origin.x + self.pos.x, 
-            self.origin.y + self.pos.y
+            self.origin.x + self.pos.x + self.border, 
+            self.origin.y + self.pos.y + self.border
         )
+        self.orientation = orientation
+        self.spacing = spacing
 
     def add(self, child):
+
+        if not self.children:
+            self.size = Size(
+                child.size.w + 2*self.border,
+                child.size.h + 2*self.border
+            )
+        else:
+            if self.orientation == "horizontal":
+                self.size = Size(
+                self.size.w + child.size.w + self.spacing, 
+                self.size.h
+            )
+            else:
+                self.size = Size(
+                self.size.w, 
+                self.size.h + child.size.h + self.spacing
+            )
+
         self.children.append(child)
         child.origin = self.next_child_origin
-        self.next_child_origin = Pos(
-            self.next_child_origin.x,
-            self.next_child_origin.y + child.size.h
-        )
+
+        if self.orientation == "horizontal":
+            self.next_child_origin = Pos(
+                self.next_child_origin.x + child.size.w + self.spacing,
+                self.next_child_origin.y
+            )
+
+        else:
+            self.next_child_origin = Pos(
+                self.next_child_origin.x,
+                self.next_child_origin.y + child.size.h + self.spacing
+            )
 
     def render(self, surface):
         rect = (
