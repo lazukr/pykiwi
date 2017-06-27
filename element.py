b140880acef36b296fcc7313799b707bcab3ae9f
lazukr <peterh10201@gmail.com>
2017-06-27 12:33:53 +0200

    added a preliminary method of doing hovers

--- element.py
+++ element.py
@@ -1,7 +1,9 @@
 import pygame
 import colour
+import hover
 
 from pos import Pos
+
 
 class Element(object):
     def __init__(self, size, pos=Pos(0, 0), background_color=colour.WHITE):
@@ -10,9 +12,43 @@ class Element(object):
         self.origin = Pos(0, 0)
         self.background_color = background_color
         self.container = None
+        self.hover_state = hover.NO_HOVER
+        self.hover_activated = False
+        self.hover_onset = 20
+        self.hover_waited = 0
+        self.hover_element = None
 
     def will_collide(self, mouse_pos):
         return self.container.collidepoint(mouse_pos)
 
+    def hover(self, mouse_pos):
+
+        if not self.hover_element:
+            return False
+
+        elif self.container.collidepoint(mouse_pos) and self.hover_state != hover.HOVER_ACTIVATED:
+            self.hover_state = hover.ON_HOVER
+        
+        elif not self.container.collidepoint(mouse_pos) and self.hover_state == hover.HOVER_ACTIVATED:
+            self.hover_state = hover.HOVER_DEACTIVATED
+            self.hover_waited = 0
+            self.hover_activated = False
+
+        if self.hover_state == hover.ON_HOVER:
+            self.hover_waited += 1
+
+        if self.hover_waited > self.hover_onset and self.hover_state != hover.HOVER_ACTIVATED:
+            self.hover_state = hover.ON_HOVER_ACTIVATED
+            self.hover_activated = True
+
+        return self.hover_state
+
+    def attach_hover(self, hover_element):
+        self.hover_element = hover_element
+
     def render(self, surface):
         raise NotImplementedError
+
+    def render_hover(self, surface):
+        pass
+
--- text.py
+++ text.py
@@ -6,9 +6,9 @@ from font import font_surface
 import colour
 import pygame
 
-class Button(Element):
-    def __init__(self, text, font_size=11):
-        super(Button, self).__init__(Size(70, 30))
+class Text(Element):
+    def __init__(self, size=Size(70, 30), text='', font_size=11):
+        super(Text, self).__init__(size)
         self.text = text
         self.font_size = font_size
         self.border = 2
@@ -21,10 +21,10 @@ class Button(Element):
             self.size.w, 
             self.size.h
         )
-        self.container = pygame.draw.rect(surface, colour.GREEN, rect)
+        self.container = pygame.draw.rect(surface, colour.WHITE, rect)
         surface.blit(
             font_surface(self.text, self.font_size),
             (abs_pos.x, abs_pos.y)
         )
 
-        self.render_hover(surface)
+
